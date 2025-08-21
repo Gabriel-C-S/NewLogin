4dd0b0ad38b26992db124533ea079ec4a390b29c
Gabriel Clayton <gabrielclayton961@gmail.com>
2025-08-21 20:10:40 -0300

    implementando o formulario no teste login

---
+++ sistema.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    //print_r($_SESSION);
+    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
+    {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('Location: index.php');
+    }
+    $logado = $_SESSION['email'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">">"
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
+    <title>Home</title>
+    <style>
+    body{
+            background-image: -o-linear-gradient(45deg, cyan, yellow);
+        }</style>
+</head>
+<body>
+    <h1>Bem-vindo ao Heleno</h1>
+    <p>Você foi logado com sucesso.</p>
+</body>
+</html>
